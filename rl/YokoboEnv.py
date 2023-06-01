5922f9a394053289111d0f1d62f46298f571a7ee
hisham246 <hishamkhalil.eng@gmail.com>
2023-06-01 13:25:17 +0900

    Revert "Trial of changing emo value"
    This reverts commit 2a014d54cef56f4399be33918d611c457b3e2bc6.

--- rl/YokoboEnv.py
+++ rl/YokoboEnv.py
@@ -313,8 +313,8 @@ class YokoboEnv(Env):
 
             random_num = random.random()
             if random_num < cst.RANDOM_DATA_EPSILON:
-                # emo = 0
-                emo = random.randint(0, len(cst.EMOTION)-3)
+                emo = 0
+                # emo = random.randint(0, len(cst.EMOTION)-3)
                 # emo = 1
 
                 temperatureIN = round(random.normalvariate(20,5),2)
