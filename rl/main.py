7d71f75dd2422c7331c192ba68a192af794d0ad0
hisham246 <hishamkhalil.eng@gmail.com>
2023-06-06 14:22:13 +0900

    Adjustments

--- rl/main.py
+++ rl/main.py
@@ -125,6 +125,9 @@ if __name__ == '__main__':
         #     episodes_to_save += 1
         # else:
         #     episodes_to_save = 0
+        
+        scores.append(score)
+        avgScore = np.mean(scores[-100:])
 
         if (avgScore > best_mean_reward):
             best_mean_reward = avgScore
@@ -148,11 +151,8 @@ if __name__ == '__main__':
         #     # printEach=True
         #     )
 
-        scores.append(score)
         epsHistory.append(agent.epsilon)
 
-        avgScore = np.mean(scores[-100:])
-        
         writer.add_scalar("reward_100", avgScore, i)
         # avgScore = np.mean(scores)
         print("episode ", i, 'score %.2f' % score,
